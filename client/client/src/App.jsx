b4cbf34236773d65a0ceed836cd81e2a4a943915
Anurag <avatsa55@gmail.com>
2026-06-06 23:16:20 +0530

    +5

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom';
 import {AuthProvider, useAuth } from './context/AuthContext';
+import Login from './pages/Login';
+
 
 const App = () => {
   return (
     <AuthProvider>
       <Routes>
-        <Route path= "/login" element={<login/>}/>
+        <Route path= "/Login" element={<Login/>}/>
       </Routes>
       
     </AuthProvider>
