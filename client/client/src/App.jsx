52336d184dfdb4899645556b59a574f156514593
Anurag <avatsa55@gmail.com>
2026-06-10 23:02:18 +0530

    +9

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom';
 import {AuthProvider, useAuth } from './context/AuthContext';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
       </Routes>
       
     </AuthProvider>
